fc538a693c3a59b689e833d3270d79ee34eff6fa
kevin-donga <dev.kevindonga@gmail.com>
2023-09-14 22:13:26 -0700

    ok done

--- lib/common_widget/common_textfield.dart
+++ lib/common_widget/common_textfield.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../utils/color_res.dart';
 
-Widget? textField({
+Widget textField({
   String? hintText,
   TextEditingController? controller,
   Widget? suffixIcon,
@@ -14,59 +14,42 @@ Widget? textField({
   bool? isDense,
   TextInputType? keyboardType,
   BoxConstraints? constraints,
-  int? minLines,
-  int? maxLines = 1,
   void Function()? onTap,
   String? Function(String?)? validator,
   void Function(String)? onChanged,
   int? maxLength,
+  String? counterText,
+  int? maxLines,
+  int? minLines,
 }) {
-  Widget textField(
-      {String? hintText,
-      TextEditingController? controller,
-      Widget? suffixIcon,
-      Widget? prefixIcon,
-      InputBorder? border,
-      Color? fillColor,
-      bool? filled,
-      required bool obscureText,
-      bool? isDense,
-      TextInputType? keyboardType,
-      BoxConstraints? constraints,
-      void Function()? onTap,
-      String? Function(String?)? validator,
-      void Function(String)? onChanged,
-      int? maxLength,
-      String? counterText}) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscureText,
-      keyboardType: keyboardType,
-      validator: validator,
-      onChanged: onChanged,
-      decoration: InputDecoration(
-        counterText: counterText,
-        border: border,
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(50),
-          borderSide: const BorderSide(color: ColorRes.blueColor),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(50),
-          borderSide: const BorderSide(color: ColorRes.greyColor),
-        ),
-        hintText: hintText,
-        suffixIcon: suffixIcon,
-        prefixIcon: prefixIcon,
-        fillColor: fillColor,
-        isDense: isDense,
-        constraints: constraints,
-        filled: filled,
+  return TextFormField(
+    controller: controller,
+    obscureText: obscureText,
+    keyboardType: keyboardType,
+    validator: validator,
+    onChanged: onChanged,
+    decoration: InputDecoration(
+      counterText: counterText,
+      border: border,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+        borderSide: const BorderSide(color: ColorRes.blueColor),
       ),
-      onTap: onTap,
-      maxLines: maxLines,
-      maxLength: maxLength,
-      minLines: minLines,
-    );
-  }
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+        borderSide: const BorderSide(color: ColorRes.greyColor),
+      ),
+      hintText: hintText,
+      suffixIcon: suffixIcon,
+      prefixIcon: prefixIcon,
+      fillColor: fillColor,
+      isDense: isDense,
+      constraints: constraints,
+      filled: filled,
+    ),
+    onTap: onTap,
+    maxLines: maxLines,
+    maxLength: maxLength,
+    minLines: minLines,
+  );
 }
